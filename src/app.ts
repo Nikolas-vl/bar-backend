3642661fbba55e0a03b8dcc0046f257204edffb8
Nikolas-vl <nikvv02@gmail.com>
2026-03-09 15:01:16 +0100

    added helmet and rate limits

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import pinoHttp from 'pino-http';
+import helmet from 'helmet';
 import routes from './routes';
 import { logger } from './utils/logger';
 import { errorHandler } from './middlewares/errorHandler';
 import { asCorsHandler, useHandlers } from './utils/express';
+import { authLimiter, apiLimiter } from './middlewares/rateLimiter';
 
 const app = express();
 
@@ -14,7 +16,12 @@ const corsMiddleware = cors({
   credentials: true,
 });
 
+app.use(helmet());
 app.use(asCorsHandler(corsMiddleware));
+
+app.use('/api/auth', authLimiter);
+app.use('/api', apiLimiter);
+
 const httpLogger = pinoHttp({ logger });
 useHandlers(app, httpLogger, express.json(), cookieParser());
 
